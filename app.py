d4e5a064fd7048da04194074ea0beafd95ebac05
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-09 10:52:04 +0300

    new

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+@app.route('/about')
+def about():
+    return "This is the about page."
+
+@app.route("/<username>")
+def username(usename):
+    return f"hello{username}"
+
+if __name__=='__main__':
+ app.run(debug=True,port=4000)
+
+
+
+
