72edefc710748b4b5d6e7707e00b24f5155dc96d
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-14 03:53:27 +0300

    contributed

--- app.py
+++ app.py
@@ -1,29 +1,57 @@
-from flask import Flask 
-from models import db, User
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
+from models import db, User, Post
+from blueprints.posts import post_pb
+from blueprints.users import user_bp
 
-
+# create the app
 app = Flask(__name__)
+
+# configure the SQLite database, relative to the app instance folder
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
 
-Migrate = Migrate(app, db)
-db.__init__(app)
+migrate = Migrate(app, db)
+
+# initialize the app with the extension
+db.init_app(app)
+
+# registering blueprint
+app.register_blueprint(user_bp, url_prefix='/api/v1')
+app.register_blueprint(post_pb, url_prefix='/api/v1')
+
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello World!"
+
 @app.route('/about')
 def about():
-    return "This is the about page."
+    return "This is about us page"
 
 @app.route("/<username>")
 def username(username):
-    return f"hello{username}"
+    return f'Hello {username}'
 
-if __name__=='__main__':
- app.run(debug=True,port=4000)
- 
- 
+
+
+
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
