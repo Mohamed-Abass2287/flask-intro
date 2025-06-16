40b68c9d47e93f98f7bd15555cc684f23f4285c1
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-16 11:03:51 +0300

    posts

--- app.py
+++ app.py
@@ -35,19 +35,46 @@ api = Api(app)
 # def username(username):
 #     return f'Hello {username}'
 
+
+#CLASS BASED ENDPOINTS
 class postEndpoint(Resource):
     def get(self):
-        posts = Post.query.all()
-        return {'posts': [post.to_dict() for post in posts]}
-
+        posts = [post.to_dict() for post in Post.query.all()]
+        return make_response({'posts': posts}, 200)
+    
     def post(self):
         data = request.get_json()
-        new_post = Post(title=data['title'], content=data['content'])
+        new_post = Post(title=data['post_title'], content=data['post_content'], user_id=data['user_id'])
         db.session.add(new_post)
         db.session.commit()
-        return {'message': 'Post created successfully'}, 201
+        return make_response({'post': new_post.to_dict()}, 201)
+    
+class postEndpointByid(Resource):
+    def get(self, post_id):
+       post = db.get_or_404(Post, post_id)
+       return make_response({'post': post.to_dict()}, 200)
+    
+    def patch(self, post_id):
+        post = db.get_or_404(Post, post_id)
+        data = request.get_json()
+        
+        for key, value in data.items():
+            setattr(post, key, value)
+        db.session.commit()
+        return make_response({'post': post.to_dict()}, 200)
 
-api.add_resource(postEndpoint, '/api/v1/posts')
+    def delete(self, post_id):
+        post = db.get_or_404(Post, post_id)
+        db.session.delete(post)
+        db.session.commit()
+        return make_response({'message': 'Post deleted successfully'}, 204)
+
+
+api.add_resource(postEndpoint, '/posts')
+api.add_resource(postEndpointByid, '/posts/<int:post_id>')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
