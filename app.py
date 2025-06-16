f80a25a8febdd7b50c2dd2232d1e22a5f30da08a
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-16 10:03:39 +0300

    post

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from models import db, User, Post
 from blueprints.posts import post_pb
 from blueprints.users import user_bp
+from flask_restful import Api, Resource
 
 # create the app
 app = Flask(__name__)
@@ -14,32 +15,42 @@ migrate = Migrate(app, db)
 
 # initialize the app with the extension
 db.init_app(app)
-
-# registering blueprint
-app.register_blueprint(user_bp, url_prefix='/api/v1')
-app.register_blueprint(post_pb, url_prefix='/api/v1')
+api = Api(app)
 
 
-@app.route('/')
-def index():
-    return "Hello World!"
-
-@app.route('/about')
-def about():
-    return "This is about us page"
-
-@app.route("/<username>")
-def username(username):
-    return f'Hello {username}'
+# # registering blueprint
+# app.register_blueprint(user_bp, url_prefix='/api/v1')
+# app.register_blueprint(post_pb, url_prefix='/api/v1')
 
 
+# @app.route('/')
+# def index():
+#     return "Hello World!"
 
+# @app.route('/about')
+# def about():
+#     return "This is about us page"
 
-    
+# @app.route("/<username>")
+# def username(username):
+#     return f'Hello {username}'
+
+class postEndpoint(Resource):
+    def get(self):
+        posts = Post.query.all()
+        return {'posts': [post.to_dict() for post in posts]}
+
+    def post(self):
+        data = request.get_json()
+        new_post = Post(title=data['title'], content=data['content'])
+        db.session.add(new_post)
+        db.session.commit()
+        return {'message': 'Post created successfully'}, 201
+
+api.add_resource(postEndpoint, '/api/v1/posts')
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
-
-
 
 
 
